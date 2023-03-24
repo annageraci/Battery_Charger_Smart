8cf4379d301b2817ae7764ca5ae89fd80c7df518
annageraci <114946337+annageraci@users.noreply.github.com>
2023-03-24 16:18:05 +0100

    Update Battery_charger_state_control.py
    finale

--- Battery_charger_state_control.py
+++ Battery_charger_state_control.py
@@ -5,8 +5,6 @@ import time
 import cherrypy
 import requests
 
-#the last
-
 class Controller:
     def __init__(self,clientID,broker, base_topic, topic_temp, topic_battery, topic_presence, topic_photon,topic_daily):
         self.clientID=clientID
