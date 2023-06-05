461a71546c01e18848d1b088d65756bf3dcdd4db
Anna <annage0709@gmail.com>
2023-06-05 21:07:34 +0200

    okay

--- Catalog_updating/UpdateCatalog.py
+++ Catalog_updating/UpdateCatalog.py
@@ -25,7 +25,6 @@ class CheckUpdate():
         response= requests.get(self.url+'/catalog')
         catalog = response.json()
         catalog['DeviceList']=self.RemainDevice
-        #json.dump(catalog, open('../CatalogFake.json', 'w'), indent=2)
         json.dump(catalog, open('../Catalog.json', 'w'), indent=2)
 
 if __name__ == '__main__':
